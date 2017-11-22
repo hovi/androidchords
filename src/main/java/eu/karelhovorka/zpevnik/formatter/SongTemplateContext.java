c1ae46ee4af6370edcc288c9929b2260466467c8
Karel Hovorka <info@karel-hovorka.eu>
2017-11-22 13:49:09 +0100

    refactor getSong -> song

---
+++ src/main/java/eu/karelhovorka/zpevnik/formatter/SongTemplateContext.java
@@ -0,0 +1,18 @@
+package eu.karelhovorka.zpevnik.formatter;
+
+
+public class SongTemplateContext {
+
+    //private final List<Section> sectionList;
+
+    private String title;
+
+    // title
+    // song
+    // chord_url_protocol:
+    // sections
+    // song display settings?
+
+
+
+}
