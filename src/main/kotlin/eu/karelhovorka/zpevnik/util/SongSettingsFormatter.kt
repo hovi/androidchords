ad99beedb42c9d728df07a1d120100fc5d7ceeea
Karel Hovorka <info@karel-hovorka.eu>
2018-01-16 23:07:31 +0100

    cleanup

--- src/main/kotlin/eu/karelhovorka/zpevnik/util/SongSettingsFormatter.kt
+++ src/main/kotlin/eu/karelhovorka/zpevnik/util/SongSettingsFormatter.kt
@@ -1,17 +1,13 @@
 package eu.karelhovorka.zpevnik.util
 
 import eu.karelhovorka.zpevnik.text.SongDisplaySettings
+import mock.JvmStatic
 
-import eu.karelhovorka.zpevnik.util.Preconditions.checkNotNull
-import mock.*
 
 object SongSettingsFormatter {
 
-
     @JvmStatic
-    fun modifyTextBasedOnSettings(source: String, settings: SongDisplaySettings): String {
-        checkNotNull(source, "source")
-        checkNotNull(settings, "settings")
+    fun modifyTextBasedOnSettings(source: String, settings: SongDisplaySettings = SongDisplaySettings.DEFAULT): String {
         var text = source
         text = ChordDetector.replacePlainChords(text)
         if (!settings.isDisplayText) {
@@ -25,6 +21,6 @@ object SongSettingsFormatter {
         } else {
             text = Transposer.removeChords(text)
         }
-        return StringUtil.removeTrailingWhitespace(text)
+        return text.trimEnd()
     }
 }
