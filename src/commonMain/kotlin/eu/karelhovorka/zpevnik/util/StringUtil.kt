6a8b78734639758745ccc5268925c63014dabed8
Karel Hovorka <info@karel-hovorka.eu>
2020-01-07 10:36:34 +0100

    allow deleting youtube link

--- src/commonMain/kotlin/eu/karelhovorka/zpevnik/util/StringUtil.kt
+++ src/commonMain/kotlin/eu/karelhovorka/zpevnik/util/StringUtil.kt
@@ -2,6 +2,13 @@ package eu.karelhovorka.zpevnik.util
 
 import eu.karelhovorka.zpevnik.util.Preconditions.checkNotNull
 
+fun String.nullIfEmpty(): String? {
+    if (isEmpty()) {
+        return null
+    }
+    return this
+}
+
 object StringUtil {
 
     fun removeTrailingWhitespace(text: String): String {
