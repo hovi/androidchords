050d0fd802b9e7881fe2c6bf7345e0c864ec495b
Karel Hovorka <git@karel-hovorka.eu>
2025-03-03 00:08:26 +0100

    Transposer tests in kotlin.

--- src/jvmTest/kotlin/eu/karelhovorka/zpevnik/util/ToneTest.kt
+++ src/jvmTest/kotlin/eu/karelhovorka/zpevnik/util/ToneTest.kt
@@ -1,57 +1,76 @@
-package eu.karelhovorka.zpevnik.util;
+package eu.karelhovorka.zpevnik.util
 
-import org.junit.Test;
+import eu.karelhovorka.zpevnik.music.Interval.Companion.of
+import eu.karelhovorka.zpevnik.util.Tone.CountryCategory
+import eu.karelhovorka.zpevnik.util.Tone.ModificationAbbreviation
+import eu.karelhovorka.zpevnik.util.ToneTransposer.Companion.transpose
+import org.junit.Assert
+import org.junit.Test
 
-import eu.karelhovorka.zpevnik.music.Interval;
-import eu.karelhovorka.zpevnik.util.Tone.CountryCategory;
-import eu.karelhovorka.zpevnik.util.Tone.ModificationAbbreviation;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
-
-public class ToneTest {
-
+class ToneTest {
     @Test
-    public void testTranspose() {
-        ToneTransposer tt = new ToneTransposer(CountryCategory.EASTERN, ModificationAbbreviation.SHARP);
-
-        assertEquals("A", tt.transpose("B", Interval.Companion.of(-1)));
-
-        assertEquals("G", tt.transpose("G#", Interval.Companion.of(-1)));
-
-        assertEquals("H", tt.transpose("C", Interval.Companion.of(-1)));
-
-        assertEquals("H", tt.transpose("H#", Interval.Companion.of(-1)));
-        assertEquals("H", tt.transpose("C", Interval.Companion.of(-1)));
-        assertNotSame("H", tt.transpose("H", Interval.Companion.of(-1)));
-
-        assertEquals("Bb", ToneTransposer.Companion.transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.FLAT, Interval.Companion.of(-1)));
-        assertEquals("A#", ToneTransposer.Companion.transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.SHARP, Interval.Companion.of(-1)));
-        assertEquals("Ais", ToneTransposer.Companion.transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.IS, Interval.Companion.of(-1)));
-        assertEquals("Bb", ToneTransposer.Companion.transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.ES, Interval.Companion.of(-1)));
+    fun testWestern() = with(ToneTransposer(CountryCategory.WESTERN, ModificationAbbreviation.SHARP)) {
 
     }
 
     @Test
-    public void testNormalize() {
-        ToneTransposer tt = new ToneTransposer(CountryCategory.EASTERN, ModificationAbbreviation.SHARP);
+    fun testTranspose() {
+        val tt = ToneTransposer(CountryCategory.EASTERN, ModificationAbbreviation.SHARP)
 
-        assertEquals("H", tt.normalize("Cb"));
-        assertEquals("H", tt.normalize("Ces"));
+        Assert.assertEquals("A", tt.transpose("B", of(-1)))
 
-        assertEquals("E", tt.normalize("Fes"));
+        Assert.assertEquals("G", tt.transpose("G#", of(-1)))
 
-        assertNotSame("Es", tt.normalize("F"));
+        Assert.assertEquals("H", tt.transpose("C", of(-1)))
+
+        Assert.assertEquals("H", tt.transpose("H#", of(-1)))
+        Assert.assertEquals("H", tt.transpose("C", of(-1)))
+        Assert.assertNotSame("H", tt.transpose("H", of(-1)))
+
+        Assert.assertEquals(
+            "Bb",
+            transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.FLAT, of(-1))
+        )
+        Assert.assertEquals(
+            "A#",
+            transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.SHARP, of(-1))
+        )
+        Assert.assertEquals(
+            "Ais",
+            transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.IS, of(-1))
+        )
+        Assert.assertEquals(
+            "Bb",
+            transpose("B", CountryCategory.WESTERN, ModificationAbbreviation.ES, of(-1))
+        )
     }
 
     @Test
-    public void testFromChordString() {
-        ToneTransposer tt = new ToneTransposer(CountryCategory.EASTERN, ModificationAbbreviation.SHARP);
+    fun testNormalize() {
+        val tt = ToneTransposer(CountryCategory.EASTERN, ModificationAbbreviation.SHARP)
 
-        assertEquals("A#", tt.fromChordString("[Bmi/Hmi]").getValue(CountryCategory.EASTERN, ModificationAbbreviation.SHARP));
+        Assert.assertEquals("H", tt.normalize("Cb"))
+        Assert.assertEquals("H", tt.normalize("Ces"))
 
-        assertEquals("D", tt.fromChordString("[Dsus4]").getValue(CountryCategory.EASTERN, ModificationAbbreviation.SHARP));
+        Assert.assertEquals("E", tt.normalize("Fes"))
 
+        Assert.assertNotSame("Es", tt.normalize("F"))
     }
 
+    @Test
+    fun testFromChordString() {
+        val tt = ToneTransposer(CountryCategory.EASTERN, ModificationAbbreviation.SHARP)
+
+        Assert.assertEquals(
+            "A#",
+            tt.fromChordString("[Bmi/Hmi]")
+                .getValue(CountryCategory.EASTERN, ModificationAbbreviation.SHARP)
+        )
+
+        Assert.assertEquals(
+            "D",
+            tt.fromChordString("[Dsus4]")
+                .getValue(CountryCategory.EASTERN, ModificationAbbreviation.SHARP)
+        )
+    }
 }
--- src/jvmTest/kotlin/eu/karelhovorka/zpevnik/util/ToneTransposerTest.kt
+++ src/jvmTest/kotlin/eu/karelhovorka/zpevnik/util/ToneTransposerTest.kt
@@ -1,122 +1,124 @@
-package eu.karelhovorka.zpevnik.util;
+package eu.karelhovorka.zpevnik.util
 
-import junit.framework.TestCase;
+import eu.karelhovorka.zpevnik.music.Interval.Companion.of
+import eu.karelhovorka.zpevnik.util.Tone.CountryCategory
+import eu.karelhovorka.zpevnik.util.Tone.ModificationAbbreviation
+import eu.karelhovorka.zpevnik.util.ToneTransposer.Companion.transposeAll
+import junit.framework.TestCase
 
-import eu.karelhovorka.zpevnik.music.Interval;
-
-
-public class ToneTransposerTest extends TestCase {
-
-    public void test7Sus4() {
-        assertEquals("[F7sus4]", transpose("[E7sus4]", 1));
+class ToneTransposerTest : TestCase() {
+    fun test7Sus4() {
+        assertEquals("[F7sus4]", transpose("[E7sus4]", 1))
     }
 
-    public void testSpecialCharChords() {
-        assertEquals("[F-]", transpose("[E-]", 1));
-        assertEquals("[F°]", transpose("[E°]", 1));
-        assertEquals("[FΔ]", transpose("[EΔ]", 1));
+    fun testSpecialCharChords() {
+        assertEquals("[F-]", transpose("[E-]", 1))
+        assertEquals("[F°]", transpose("[E°]", 1))
+        assertEquals("[FΔ]", transpose("[EΔ]", 1))
     }
 
-    public void testTransposeB() {
-        Tone.CountryCategory countryCategory = Tone.CountryCategory.EASTERN;
-        Tone.ModificationAbbreviation modificationAbbreviation = Tone.ModificationAbbreviation.FLAT;
-        assertEquals("[Ab]", transpose("[Bb]", -1, countryCategory, modificationAbbreviation));
-        assertEquals("[A]", transpose("[B]", -1, countryCategory, modificationAbbreviation));
-        assertEquals("[B]", transpose("[Bb]", 1, countryCategory, modificationAbbreviation));
+    fun testTransposeB() {
+        val countryCategory = CountryCategory.EASTERN
+        val modificationAbbreviation = ModificationAbbreviation.FLAT
+        assertEquals("[Ab]", transpose("[Bb]", -1, countryCategory, modificationAbbreviation))
+        assertEquals("[A]", transpose("[B]", -1, countryCategory, modificationAbbreviation))
+        assertEquals("[B]", transpose("[Bb]", 1, countryCategory, modificationAbbreviation))
     }
 
-    public void testTransposePlus() {
-        assertEquals("[G#6+]", transpose("[A6+]", -1));
-        assertEquals("[G#6+]", transpose("[G#6+]", 0));
-        assertEquals("[G#6+]", transpose("[G6+]", 1));
-
+    fun testTransposePlus() {
+        assertEquals("[G#6+]", transpose("[A6+]", -1))
+        assertEquals("[G#6+]", transpose("[G#6+]", 0))
+        assertEquals("[G#6+]", transpose("[G6+]", 1))
     }
 
-    public void testTransposeDoubles() {
+    fun testTransposeDoubles() {
+        assertEquals("[Bmi/Hmi]", transpose("[Bmi/Hmi]", 0))
 
-        assertEquals("[Bmi/Hmi]", transpose("[Bmi/Hmi]", 0));
+        assertEquals("[Ami/A#mi]", transpose("[Bmi/Hmi]", -1))
 
-        assertEquals("[Ami/A#mi]", transpose("[Bmi/Hmi]", -1));
+        assertEquals("[Gmi/Ami]", transpose("[G#mi/A#mi]", -1))
 
-        assertEquals("[Gmi/Ami]", transpose("[G#mi/A#mi]", -1));
+        assertEquals("[H/G#]", transpose("[C/A]", -1))
 
-        assertEquals("[H/G#]", transpose("[C/A]", -1));
+        assertEquals("[A#mi/G#]", transpose("[Hmi/A]", -1))
 
-        assertEquals("[A#mi/G#]", transpose("[Hmi/A]", -1));
-
-        assertEquals("[G] [E] [F#/H]", transpose("[G# F G/C]", -1));
-
+        assertEquals("[G] [E] [F#/H]", transpose("[G# F G/C]", -1))
     }
 
-    public void testTranspose() {
+    fun testTranspose() {
+        assertEquals("[Cmi7/5]", transpose("[Hmi7/5]", 1))
 
-        assertEquals("[Cmi7/5]", transpose("[Hmi7/5]", 1));
+        assertEquals("[Fmi7/11]", transpose("[F#mi7/11]", 11))
+        assertEquals("[Fmi7/11]", transpose("[F#mi7/11]", -1))
 
-        assertEquals("[Fmi7/11]", transpose("[F#mi7/11]", 11));
-        assertEquals("[Fmi7/11]", transpose("[F#mi7/11]", -1));
+        assertEquals("[F5/13]", transpose("[F5/13]", 12))
 
-        assertEquals("[F5/13]", transpose("[F5/13]", 12));
+        assertEquals("[A#]", transpose("[A#]", 0))
 
-        assertEquals("[A#]", transpose("[A#]", 0));
+        assertEquals("[B]", transpose("[B]", 0))
 
-        assertEquals("[B]", transpose("[B]", 0));
+        assertEquals("[Hadd13]", transpose("[Cadd13]", -1))
 
-        assertEquals("[Hadd13]", transpose("[Cadd13]", -1));
+        assertEquals("[Emaj7]", transpose("[Fmaj7]", -1))
 
-        assertEquals("[Emaj7]", transpose("[Fmaj7]", -1));
+        assertEquals("[E]", transpose("[F]", -1))
 
-        assertEquals("[E]", transpose("[F]", -1));
+        assertEquals("[Em]", transpose("[Fm]", -1))
 
-        assertEquals("[Em]", transpose("[Fm]", -1));
+        assertEquals("[Em7]", transpose("[Fm7]", -1))
 
-        assertEquals("[Em7]", transpose("[Fm7]", -1));
+        assertEquals("[Emi7]", transpose("[Fmi7]", -1))
 
-        assertEquals("[Emi7]", transpose("[Fmi7]", -1));
+        assertEquals("[Esus]", transpose("[Fsus]", -1))
 
-        assertEquals("[Esus]", transpose("[Fsus]", -1));
+        assertEquals("[Esus2]", transpose("[Fsus2]", -1))
 
-        assertEquals("[Esus2]", transpose("[Fsus2]", -1));
+        assertEquals("[Esus4]", transpose("[Fsus4]", -1))
 
-        assertEquals("[Esus4]", transpose("[Fsus4]", -1));
+        assertEquals("[F#maj7]", transpose("[Gmaj7]", -1))
 
-        assertEquals("[F#maj7]", transpose("[Gmaj7]", -1));
+        assertEquals("[G]", transpose("[Ab]", -1))
 
-        assertEquals("[G]", transpose("[Ab]", -1));
+        assertEquals("[G#]", transpose("[G]", 1))
 
-        assertEquals("[G#]", transpose("[G]", 1));
+        assertEquals("[G] [E] [F#]", transpose("[G# F G]", -1))
 
-        assertEquals("[G] [E] [F#]", transpose("[G# F G]", -1));
-
-        assertEquals("1", "1,2".split("[,\\s*]")[0]);
+        assertEquals(
+            "1",
+            "1,2".split("[,\\s*]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
+        )
     }
 
-    public void testMultiBracket() {
-        assertEquals("[D#mi] [C#] [G#mi] X", transpose("[Emi D Ami ]X", -1));
+    fun testMultiBracket() {
+        assertEquals("[D#mi] [C#] [G#mi] X", transpose("[Emi D Ami ]X", -1))
 
-        assertEquals(" [D#mi] [C#] [G#mi]", transpose("[ Emi D Ami]", -1));
+        assertEquals(" [D#mi] [C#] [G#mi]", transpose("[ Emi D Ami]", -1))
 
-        assertEquals("[D#mi] [C#] [G#mi]", transpose("[Emi D Ami]", -1));
+        assertEquals("[D#mi] [C#] [G#mi]", transpose("[Emi D Ami]", -1))
 
-        assertEquals("[D#mi] [C#] [G#mi]", transpose("[Emi, D, Ami]", -1));
+        assertEquals("[D#mi] [C#] [G#mi]", transpose("[Emi, D, Ami]", -1))
 
-        assertEquals("[D#mi][C#][G#mi]", transpose("[Emi,D,Ami]", -1));
+        assertEquals("[D#mi][C#][G#mi]", transpose("[Emi,D,Ami]", -1))
     }
 
-    public void testFailedValues() {
-        assertEquals("[x#]", transpose("[x#]", 0));
-        assertEquals("[X#]", transpose("[X#]", 0));
-        assertEquals("[x#]", transpose("[x#]", 1));
-        assertEquals("[X#]", transpose("[X#]", 1));
-        assertEquals("[XFDFDFDSFDS#]", transpose("[XFDFDFDSFDS#]", 0));
-        assertEquals("[XFDFDFDSFDS#]", transpose("[XFDFDFDSFDS#]", 1));
-
+    fun testFailedValues() {
+        assertEquals("[x#]", transpose("[x#]", 0))
+        assertEquals("[X#]", transpose("[X#]", 0))
+        assertEquals("[x#]", transpose("[x#]", 1))
+        assertEquals("[X#]", transpose("[X#]", 1))
+        assertEquals("[XFDFDFDSFDS#]", transpose("[XFDFDFDSFDS#]", 0))
+        assertEquals("[XFDFDFDSFDS#]", transpose("[XFDFDFDSFDS#]", 1))
     }
 
-    public static String transpose(String text, int step, Tone.CountryCategory countryCategory, Tone.ModificationAbbreviation modificationAbbreviation) {
-        return ToneTransposer.Companion.transposeAll(text, Interval.Companion.of(step), countryCategory, modificationAbbreviation);
-    }
-
-    public static String transpose(String text, int step) {
-        return transpose(text, step, Tone.CountryCategory.EASTERN, Tone.ModificationAbbreviation.SHARP);
+    companion object {
+        @JvmOverloads
+        fun transpose(
+            text: String,
+            step: Int,
+            countryCategory: CountryCategory = CountryCategory.EASTERN,
+            modificationAbbreviation: ModificationAbbreviation = ModificationAbbreviation.SHARP
+        ): String {
+            return transposeAll(text, of(step), countryCategory, modificationAbbreviation)
+        }
     }
 }
